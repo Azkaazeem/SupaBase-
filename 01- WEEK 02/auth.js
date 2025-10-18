8aeb563ba27521886c0c084dd60183992ca5ed4f
azkaazeem <azkaazeem804@gmail.com>
2025-10-19 03:47:49 +0500

    deletee random line

---
+++ 01- WEEK 02/auth.js
@@ -0,0 +1,16 @@
+import supaBase from "./config.js"
+
+console.log(supaBase);
+
+let regform = document.getElementById ('regform');
+let sfullName = document.getElementById ('s-full-name');
+let sEmail = document.getElementById ('s-email');
+let spassword = document.getElementById ('s-password');
+let sPhoneNo = document.getElementById ('s-phone-no');
+
+// console.log(regform);
+// console.log(sfullName);
+// console.log(sEmail);
+// console.log(spassword);
+// console.log(sPhoneNo);
+
