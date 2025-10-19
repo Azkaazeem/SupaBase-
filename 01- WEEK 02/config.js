4173d6254f5a37135f92802a7aecf4d29ed2d34a
azkaazeem <azkaazeem804@gmail.com>
2025-10-19 05:10:40 +0500

    Authentication Done

--- 01- WEEK 02/config.js
+++ 01- WEEK 02/config.js
@@ -8,3 +8,4 @@ const supaBaseKEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS
 const supaBase = createClient('https://njkujgyrnopzyriirmoi.supabase.co' , 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qa3VqZ3lybm9wenlyaWlybW9pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA3MzQyOTUsImV4cCI6MjA3NjMxMDI5NX0.J3NsnRpwYI2T0e4YIK5kooK5g7GobYNS_11fDz_XR3g');
 
 export default supaBase;
+
