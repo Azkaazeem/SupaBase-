3f214e7c67b76e7535c22ecc52397aed980f3e5a
azkaazeem <azkaazeem804@gmail.com>
2025-10-19 05:07:30 +0500

    deletee random line

--- 01- WEEK 02/auth.js
+++ 01- WEEK 02/auth.js
@@ -2,15 +2,96 @@ import supaBase from "./config.js"
 
 console.log(supaBase);
 
-let regform = document.getElementById ('regform');
-let sfullName = document.getElementById ('s-full-name');
-let sEmail = document.getElementById ('s-email');
-let spassword = document.getElementById ('s-password');
-let sPhoneNo = document.getElementById ('s-phone-no');
+let regform = document.getElementById('regform');
+let sfullName = document.getElementById('s-full-name');
+let sEmail = document.getElementById('s-email');
+let spassword = document.getElementById('s-password');
+let sPhoneNo = document.getElementById('s-phone-no');
 
 // console.log(regform);
 // console.log(sfullName);
 // console.log(sEmail);
 // console.log(spassword);
 // console.log(sPhoneNo);
+
+async function signUp(e) {
+    e.preventDefault()
+    try {
+
+        if (!sEmail.value) {
+            alert("Plzzzz Enter Your Email.......");
+        }
+
+        if (!spassword.value) {
+            alert("Plzzzz Enter Your Password.......");
+        }
+
+        const { data, error } = await supaBase.auth.signUp(
+            {
+                email: sEmail.value,
+                password: spassword.value,
+                options: {
+                    data: {
+                        first_name: sfullName.value,
+                        PhoneNo: sPhoneNo.value,
+                    }
+                }
+            }
+        )
+
+        if (error) {
+            console.log(error);
+        }
+        else {
+            location.href = "wel.html";
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+regform && regform.addEventListener("submit", signUp);
+
+
+// ==================================================LOGIN PAGE==================================================
+
+let lSignin = document.getElementById('l-form');
+let lEmail = document.getElementById('l-email');
+let lpassword = document.getElementById('l-password');
+
+// console.log(lSignin);
+// console.log(sEmail);
+// console.log(spassword);
+
+async function SignIn(e) {
+    e.preventDefault()
+    try {
+
+        if (!lEmail.value) {
+            alert("Plzzzz Enter Your Email.......");
+        }
+
+        if (!lpassword.value) {
+            alert("Plzzzz Enter Your Password.......");
+        }
+
+        const { data, error } = await supaBase.auth.signInWithPassword({
+            email: lEmail.value,
+            password: lpassword.value,
+        })
+
+        if (error) {
+            console.log(error);
+        }
+        else {
+            location.href = "wel.html";
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+lSignin && lSignin.addEventListener("submit", SignIn);
 
